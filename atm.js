f3b0b458a61007ee57170a023ef99396ab36a02b
Kevin Wilburn <wilburnkevin200@gmail.com>
2021-05-19 21:15:45 -0400

    Initial push of ATM project

---
+++ atm.js
@@ -0,0 +1,21 @@
+const account = require("./account");
+
+
+
+
+
+
+function getBalance(){
+}
+function withdraw(){
+}
+function deposit(){
+}
+function validatePin(){
+}
+
+
+module.exports.getBal = getBalance;
+module.exports.wdraw = withdraw;
+module.exports.dep = deposit;
+module.exports.valPin = validatePin;
---
+++ index.js
@@ -0,0 +1,1 @@
+const atm = require("./atm");
